543e3b38c4b0e5227d89d737e153bc6c49bc09d5
Guilherme Figueiroa <guilherme.mfigueiroa@gmail.com>
2016-05-16 22:43:18 -0300

    Created and updated fields for type

--- grails-app/domain/gui/commerce/ProductType.groovy
+++ grails-app/domain/gui/commerce/ProductType.groovy
@@ -3,8 +3,10 @@ package gui.commerce
 class ProductType {
 
     String name
+    Date dateCreated
+    Date lastUpdated
 
     static constraints = {
-      name nullable:true, blank: false, maxSize: 64, unique: true
+      name nullable:false, blank: false, maxSize: 64, unique: true
     }
 }
